0a89ceb82eaf064bd0f38fac7eee0d493e595fcc
Aditi Panigrahi <aditi@Aditis-MacBook-Air.local>
2024-03-25 00:01:33 -0700

    Add initial project commit

---
+++ CloudflareAssessment/src/main/java/com/example/CloudflareAssessment/CloudflareAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.CloudflareAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflareAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudflareAssessmentApplication.class, args);
+	}
+
+}
---
+++ CloudflareAssessment/src/test/java/com/example/CloudflareAssessment/CloudflareAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CloudflareAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudflareAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
